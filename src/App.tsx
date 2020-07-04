b0a56b6588f26fed8d7ca72bbd0c996a4fa876d5
svrsaljko <vrsaljko tjepan@gmail.com>
2020-07-04 02:01:50 +0200

    update project from js to ts

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      hello
+    </div>
+  );
+}
+
+export default App;

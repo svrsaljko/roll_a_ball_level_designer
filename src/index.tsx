1e88f4c7e9a13bea324321e95bc0f83626ca1b67
svrsaljko <vrsaljko tjepan@gmail.com>
2020-08-25 19:56:36 +0200

    add redux-persist, package for storing to local storage

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,25 @@ import App from './App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from '../src/reducers';
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 
-const store = createStore(rootReducer);
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store: any = createStore(persistedReducer);
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
     ,
   </React.StrictMode>,
